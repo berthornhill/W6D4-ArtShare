005bae77d91b0cfb962d73202d149d17516a49c2
berthornhill <64211468+berthornhill@users.noreply.github.com>
2020-12-03 16:31:30 -0500

    Added associations to models.

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -10,4 +10,18 @@
 class Artwork < ApplicationRecord
     validates :artist_id, uniqueness: {scope: :title, 
         message: 'This title is taken'}
+
+    has_many :shares,
+    primary_key: :id,
+    foreign_key: :artwork_id,
+    class_name: :ArtworkShare
+
+    belongs_to :user,
+    primary_key: :id,
+    foreign_key: :artist_id,
+    class_name: :User
+
+    has_many :shared_viewers,
+    through: :shares,
+    source: :viewer
 end
--- app/models/artworkshare.rb
+++ app/models/artworkshare.rb
@@ -1,3 +1,14 @@
 class ArtworkShare < ApplicationRecord
     validates presence: true
+
+    belongs_to :artwork,
+    primary_key: :id,
+    foreign_key: :artwork_id,
+    class_name: :Artwork
+
+    belongs_to :viewer,
+    primary_key: :id,
+    foreign_key: :viewer_id,
+    class_name: :User
+    
 end
