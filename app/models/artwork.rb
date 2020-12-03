c71d5302fa33f4d4e6720e77751f52ae7dc9573a
stephen <stephenhyim@gmail.com>
2020-12-03 15:44:45 -0500

    ArtworkShare & Artwork model and db

---
+++ app/models/artwork.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: artworks
+#
+#  id        :bigint           not null, primary key
+#  title     :string           not null
+#  image_url :string           not null
+#  artist_id :integer          not null
+#
+class Artwork < ApplicationRecord
+    validates :artist_id, uniqueness: {scope: :title, 
+        message: 'This title is taken'}
+end
---
+++ app/models/artworkshare.rb
@@ -0,0 +1,3 @@
+class ArtworkShare < ApplicationRecord
+    validates presence: true
+end
